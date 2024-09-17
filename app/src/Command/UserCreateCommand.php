fa40decd5837baa45983c750204021eb9b9dc8af
Vladyslav Drybas <ukdvvw@gmail.com>
2024-09-17 17:54:58 +0200

    aadd store user from command

--- app/src/Command/UserCreateCommand.php
+++ app/src/Command/UserCreateCommand.php
@@ -3,17 +3,18 @@
 namespace App\Command;
 
 use App\Builder\UserBuilder;
-use DateTime;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Faker\Factory;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 #[AsCommand(
     name: 'user:create',
@@ -31,6 +32,7 @@ class UserCreateCommand extends Command
     }
 
     protected function configure(): void {
+        $this->addArgument('amount', InputArgument::OPTIONAL);
         $this->addArgument('email', InputArgument::OPTIONAL);
         $this->addArgument('password', InputArgument::OPTIONAL);
         $this->addArgument('username', InputArgument::OPTIONAL);
@@ -40,20 +42,57 @@ class UserCreateCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $io->info('Project environment:' . $this->projectEnvironment);
-
-        $io->success('Success');
+        $io->warning('ENVIRONMENT: ' . strtoupper($this->projectEnvironment));
 
         $faker = Factory::create();
 
+        $amount = filter_var($input->getArgument('amount') ?? 1, FILTER_VALIDATE_INT);
+        $progressBar = new ProgressBar($output, $amount);
+        $progressBar->start();
+
+
+        $counter = 0;
         $email = $input->getArgument('email') ?? $faker->email();
         $password = $input->getArgument('password') ?? 123123;
         $username = $input->getArgument('username') ?? $faker->userName();
+        $outputRows = [];
+        do {
+            $user = $this->userBuilder->base($email, $password, $username);
 
-        $user = $this->userBuilder->base($email, $password, $username);
+            try {
+                $userRepository = $this->em->getRepository(User::class);
+                $userRepository->add($user);
+                $userRepository->save();
+            } catch (Exception $e) {
+                $io->error($e->getMessage());
+            }
 
-        dump($user);
+            $counter++;
 
+            $outputRows[] = [
+                $counter,
+                $user->getUsername(),
+                $user->getEmail(),
+                $password
+            ];
+
+            $email = $faker->email();
+            $password = 123123;
+            $username = $faker->userName();
+
+
+            $progressBar->advance();
+        } while ($counter < $amount);
+        $progressBar->finish();
+
+        $table = new Table($output);
+        $table
+            ->setHeaders(['#', 'Username', 'Email', 'Password'])
+            ->setRows($outputRows)
+        ;
+        $table->render();
+
+        $io->success($amount . ' users created!');
 
         return Command::SUCCESS;
     }
