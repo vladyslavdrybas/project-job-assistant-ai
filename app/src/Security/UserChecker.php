76fe22369bec881d1e5a171d52d75eb12fa77ddf
Vladyslav Drybas <ukdvvw@gmail.com>
2024-09-19 22:06:38 +0200

    fix security

--- app/src/Security/UserChecker.php
+++ app/src/Security/UserChecker.php
@@ -5,27 +5,17 @@ declare(strict_types=1);
 namespace App\Security;
 
 use App\Entity\User;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Exception;
 use Symfony\Component\Security\Core\Exception\LockedException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserChecker implements UserCheckerInterface
 {
-    protected FlashBagInterface $flash;
-
-    public function __construct(
-        protected readonly RequestStack $requestStack
-    ) {
-        $this->flash = $this->requestStack->getSession()->getFlashBag();
-    }
-
     public function checkPreAuth(UserInterface $user): void
     {
-//        $this->flash->set('danger', __METHOD__);
         if (!$user instanceof User) {
-            return;
+            throw new Exception('Not a User instance.');
         }
 
         if ($user->isDeleted()) {
@@ -35,6 +25,5 @@ class UserChecker implements UserCheckerInterface
 
     public function checkPostAuth(UserInterface $user): void
     {
-//        $this->flash->set('danger', __METHOD__);
     }
 }
